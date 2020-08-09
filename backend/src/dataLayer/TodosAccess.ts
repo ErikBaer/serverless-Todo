8f5d916d6e8998c3c7db42cdea43cc131a3ba0f6
erikbaer <baerdata.ger@googlemail.com>
2020-08-09 20:02:16 +0200

    create dataLayer

--- backend/src/dataLayer/TodosAccess.ts
+++ backend/src/dataLayer/TodosAccess.ts
@@ -1,23 +1,47 @@
-import * as AWS from 'aws-sdk'
-import { createLogger } from '../../utils/logger'
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
+const XAWS = AWSXRay.captureAWS(AWS)
 
-import {TodoItem} from '../models/TodoItem'
-
-const logger = createLogger('getTodos')
+import { TodoItem } from '../models/TodoItem'
 
 export class TodosAccess {
 
-constructor (
-    private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
-    private readonly todosTable = process.env.TODOS_TABLE){
+  constructor(
+    private readonly docClient: DocumentClient = createDynamoDBClient(),
+    private readonly todosTable = process.env.TODOS_TABLE) {
+  }
 
-    }
+  async getAllGroups(): Promise<TodoItem[]> {
+    console.log('Getting all groups')
 
-async getAllTodos(): Promise<TodoItem[]> {
-    logger.info('Getting all Todos')
+    const result = await this.docClient.scan({
+      TableName: this.todosTable
+    }).promise()
 
-    const result = await 
+    const items = result.Items
+    return items as TodoItem[]
+  }
+
+  async createTodo(todo: TodoItem): Promise<TodoItem> {
+    await this.docClient.put({
+      TableName: this.todosTable,
+      Item: todo
+    }).promise()
+
+    return todo
+  }
 }
 
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+    console.log('Creating a local DynamoDB instance')
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+
+  return new XAWS.DynamoDB.DocumentClient()
 }
